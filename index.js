8af7d367dd0af35cab49a06458dd6b5cf5671102
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-28 17:11:52 +0600

    create delete api for delete menuitem

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -41,6 +41,18 @@ async function run() {
         console.log(err)
     }
 
+    try{
+      app.delete('/deleteMenu/:id',async(req,res)=>{
+        const id = req.params.id;
+        console.log(id)
+        const query = {_id: (id)}
+        const result = await menuCollection.deleteOne(query)
+        res.send(result)
+      })
+    }catch(err){
+      console.log(err)
+    }
+
     // post addto cart
     try{
     app.post('/addToCart',async(req,res)=>{
@@ -61,6 +73,8 @@ async function run() {
     }catch(err){
       console.log(err)
     }
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
